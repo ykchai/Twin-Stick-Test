fcfaa260a864f70989365b82c2f67879cd8d4e3e
ykchai <34904452+ykchai@users.noreply.github.com>
2018-01-02 15:02:41 +0800

    New Replay Class

---
+++ Twin-Stick-Test/Assets/MyKeyFrame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyKeyFrame : MonoBehaviour {
+
+    float time;
+    Vector3 pos;
+    Quaternion rot;
+
+    public MyKeyFrame( float _time, Vector3 _pos, Quaternion _rot )
+    {
+        _time = this.time;
+        _pos = this.pos;
+        _rot = this.rot;
+    }
+}
